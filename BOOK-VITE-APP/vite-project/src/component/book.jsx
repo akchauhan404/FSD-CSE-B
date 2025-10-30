6146004856d7b22653105b80afe5d8df98dfe14d
Atul <akchauhan071@gmail.com>
2025-10-30 15:35:38 +0530

    created book vite app

---
+++ BOOK-VITE-APP/vite-project/src/component/book.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const book = () => {
+  return (
+    <div>
+      <img src="" width="200" height="200" alt="image"/>
+      <h3>Title: Physics</h3>
+      <h4>Price: 761</h4>
+      <button>Add to Cart</button>
+    </div>
+  )
+}
+
+export default book
